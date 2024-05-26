ddbbb72a1d3563172b54feba87ad82eba21e1973
Shivam Pundir <shiv.pundir1992@gmail.com>
2024-05-26 20:37:24 +0100

    scrapped jobs from efinancials

--- search_jobs.py
+++ search_jobs.py
@@ -3,7 +3,7 @@ import pandas as pd
 import json
 import os
 import warnings
-
+from datetime import datetime
 warnings.filterwarnings("ignore")
 
 directory = r'C:\Users\shivampundir\Downloads\linkedin\efinancials'
@@ -17,7 +17,8 @@ pg = 1
 while True:
 	
 	url = 'https://job-search-api.efinancialcareers.com/v1/efc/jobs/search?location=London,%20UK&countryCode2=GB&page={}&pageSize=100'.format(pg)
-	response = requests.get(url,verify=False)
+	#url = 'https://job-search-api.efinancialcareers.com/v1/efc/jobs/search?location=ASIA&EUROPE&GULF&countryCode2=GB&page={}&pageSize=200'.format(pg)
+    response = requests.get(url,verify=False)
 
 	json_data = response.json()
 	meta_data = json_data['meta']
@@ -29,12 +30,12 @@ while True:
 	data_list.append(data)
 	current_page = meta_data['currentPage']
 	print(f' Completed page number {current_page}')
-    pg = pg + 1
-	if pg > meta_data['pageCount']:
+	pg = pg + 1
+    if pg > meta_data['pageCount']:
         break
-		
+
 		
 jobs_df = pd.concat(data_list,axis=0)
 jobs_df = jobs_df.reset_index(drop=True)
 jobs_df['jobUrl'] = 'https://www.efinancialcareers.co.uk'+jobs_df['detailsPageUrl']
-jobs_df.to_excel('London-Jobs.xlsx')
+jobs_df.to_excel(f'London-Jobs_asof_{datetime.now().date().strftime("%Y-%m-%d")}.xlsx')
